4fe92738a4aabb0577bcb62928cbd813fbecf823
aurel <aurelie.chazelle@gmail.com>
2025-05-22 13:31:41 +0200

    Debug: local cloudinary youtube video 1.0: import setting

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,10 @@ import os
 import sys
 import dj_database_url
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
